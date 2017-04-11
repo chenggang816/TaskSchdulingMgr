01b3be639bfcf955b0a5b292a13d4a5fe867c380
cg <1305753112@qq.com>
2017-04-11 22:22:24 +0800

    asynchronized test service.

--- src/com/appmain/FrameMain.java
+++ src/com/appmain/FrameMain.java
@@ -88,12 +88,21 @@ public class FrameMain extends JFrame{
 				if(ipList.size() <= 0){
 					return;
 				}
-				Map<String, Boolean> map;
-				map = SocketHelper.tryCommunicate(ipList, 8000);
-				for(int i = 0;i < ipList.size();i++){
-					String r = map.get(ipList.get(i)) ? "是":"否";
-					model.setValueAt(r, i, 3);
-				}
+				new Thread(){
+					public void run(){
+						String strBtnTxt = btnTestService.getText();
+						btnTestService.setText("正在测试服务");
+						btnTestService.setEnabled(false);
+						Map<String, Boolean> map;
+						map = SocketHelper.tryCommunicate(ipList, 8000);
+						for(int i = 0;i < ipList.size();i++){
+							String r = map.get(ipList.get(i)) ? "是":"否";
+							model.setValueAt(r, i, 3);
+						}
+						btnTestService.setText(strBtnTxt);
+						btnTestService.setEnabled(true);
+					}
+				}.start();
 			}
 		});
 		
--- src/com/nettools/SocketHelper.java
+++ src/com/nettools/SocketHelper.java
@@ -8,25 +8,58 @@ import java.util.Map;
 
 import com.socket.Client;
 
+
+
 public class SocketHelper {
+	class Monitor{
+		public Monitor(int count){
+			this.count = count;
+		}
+		private int count;
+		synchronized public void reduceCount(int count){
+			this.count -= count;
+		}
+		public int getCount(){
+			System.out.println(this.count);
+			return this.count;
+		}
+	}
 	/*
 	 * 测试ips里的每个ip对应port端口的服务是否开启
 	 * 返回的Map的key为ip,value为true表示成功开启
 	 */
-	public static Map<String, Boolean> tryCommunicate(List<String> ips,int port){
-		Map<String,Boolean> map = new HashMap<String,Boolean>();
-		for(String ip:ips){
-			Client client = null;
-			try {
-				client = new Client(ip,port);
-			} catch (IOException e) {
-				e.printStackTrace();
+	public static Map<String, Boolean> tryCommunicate(List<String> ips,final int port){
+		final Map<String,Boolean> map = new HashMap<String,Boolean>();
+		final Monitor monitor = new SocketHelper().new Monitor(ips.size());
+		for(final String ip:ips){
+			new Thread(){
+				public void run(){
+					Client client = null;
+					try {
+						client = new Client(ip,port);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					if(client.sendHello() == null){
+						map.put(ip, false);
+					}else{
+						map.put(ip, true);
+					}
+					monitor.reduceCount(1);
+					if(monitor.getCount() <= 0){
+						synchronized(monitor){
+							monitor.notify();
+						}
+					}
+				}
+			}.start();
+		}
+		try {
+			synchronized(monitor){
+				monitor.wait();
 			}
-			if(client.sendHello() == null){
-				map.put(ip, false);
-			}else{
-				map.put(ip, true);
-			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 		return map;
 	}
